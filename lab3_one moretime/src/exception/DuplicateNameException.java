b7fd1d1d2c4f7f5874140cab53347d1daf6a15ab
sonimoo <145650788+sonimoo@users.noreply.github.com>
2025-02-11 21:57:18 +0200

    Delete lab3_one moretime directory

--- lab3_one moretime/src/exception/DuplicateNameException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class DuplicateNameException extends Exception {
-    public DuplicateNameException(String name) {
-        super("Ошибка: Имя '" + name + "' уже существует.");
-    }
-}
--- lab3_one moretime/src/exception/InvalidNameException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class InvalidNameException extends Exception {
-    public InvalidNameException(String name) {
-        super("Ошибка: Имя должно состоять из букв!");
-    }
-}
